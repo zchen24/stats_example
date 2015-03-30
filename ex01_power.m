0989777d0b3eaebbffd16e2e8e260d0a59b9e73b
Zihan Chen <zihan.chen@jhu.edu>
2015-03-30 14:07:21 -0400

    updated power analysis reference

---
+++ ex01_power.m
@@ -0,0 +1,27 @@
+%% Others: Power Analysis
+%
+% * Power Analysis I: <https://youtu.be/GOqRaQ5d2VI Basic Concept>
+% * Power Analysis II: <https://youtu.be/fRm2dEWSJrk Example>
+% * MATLAB: <http://www.mathworks.com/help/stats/sampsizepwr.html sampsizepwr>
+% 
+%  * Zihan Chen
+%  * 2015-03-29
+
+%% Power
+% * Type-II Error: beta
+% * power: 1 - beta
+
+clc; clear;
+
+
+
+
+%% Power Analysis 
+
+
+
+
+
+
+
+
